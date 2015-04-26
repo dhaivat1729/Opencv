0b5987781047cbe620064d786f00e0df5125b8d5
dhaivat666 <dhaivat1994@gmail.com>
2015-04-26 19:29:24 +0530

    Changes made to Brightness and contrast control program.

--- TheCoreModule/BrightnessAndContrast/BrightnessAndContrast.cpp
+++ TheCoreModule/BrightnessAndContrast/BrightnessAndContrast.cpp
@@ -7,29 +7,40 @@
 using namespace std;
 using namespace cv;
 
-double alpha; // For contrast control
-double beta; // For brightness control
+double alpha; // to control the brightness
+double beta; // to control the contrast
 
-int main( int argc, char** argv){
+int main( int argc, char** argv ){
 
     Mat src, final;
-    src = imread( argv[1], CV_LOAD_IMAGE_COLOR);
-    final = Mat::zeros(src.size(), src.type());
-    cout << "Enter the value of alpha[1.0 - 3.0]: ";
-    cin >> alpha;
-    cout << "Enter the value of beta[0.0 - 100.0]: ";
-    cin >> beta;
-    for(int y = 0;y < src.rows; y++){
-        for(int x = 0;x < src.cols; x++){
-            for( int c=0; c < 3; c++){
+    if ( argc != 2){
+        cout << "Usage : BrighnessAndContrast <Image path> " << endl;
+    }
 
-                final.at<Vec3b>(y,x)[c] = ( alpha*(src.at<Vec3b>(y,x)[c]) + beta );
+    src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    final = Mat::zeros(src.size(),src.type());
+    if( !src.data ){
+        cout << "No valid image data!" << endl;
+    }
+
+    // Take the values of alpha and beta from user
+    cout << "Enter a value to control contrast: " << endl;
+    cin >> alpha;
+    cout << "Enter a value to control brighness: " << endl;
+    cin >> beta;
+
+    for( int y = 0; y < src.rows; y++){
+        for( int x = 0; x < src.cols; x++){
+            for( int c = 0; c<3; c++){
+                final.at<Vec3b>(y,x)[c] = ( alpha * src.at<Vec3b>(y,x)[c] + beta );
             }
+
         }
     }
 
-    namedWindow("Final image", CV_WINDOW_AUTOSIZE);
-    imshow("Final image", final);
+    namedWindow( "Final Image Display",CV_WINDOW_AUTOSIZE);
+    imshow( "FInal Image Display", final);
     waitKey(0);
-    return 0;
+    return 0;   
+
 }
