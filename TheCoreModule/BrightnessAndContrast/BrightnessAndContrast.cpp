ae112a34850184fa4a915820e3685024c6b613d7
dhaivat666 <dhaivat1994@gmail.com>
2015-04-28 04:07:47 +0530

    Initialized loop variables to make code functioning.

--- TheCoreModule/BrightnessAndContrast/BrightnessAndContrast.cpp
+++ TheCoreModule/BrightnessAndContrast/BrightnessAndContrast.cpp
@@ -4,43 +4,43 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
-double alpha; // to control the brightness
-double beta; // to control the contrast
+double alpha;  // to control the contrast
+double beta; // to control the brightness
 
-int main( int argc, char** argv ){
+int main( int argc, char** argv){
 
-    Mat src, final;
-    if ( argc != 2){
-        cout << "Usage : BrighnessAndContrast <Image path> " << endl;
+    if(argc != 2){
+
+        cout << "Usage: BrightnessAndContrast <Image path>" << endl;
     }
+        Mat src, final;
+        src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+        final = Mat::ones(src.size(), src.type());
 
-    src = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    final = Mat::zeros(src.size(),src.type());
-    if( !src.data ){
-        cout << "No valid image data!" << endl;
-    }
+        cout << "Provide the value of alpha: ";
+        cin >> alpha;
+        cout << endl;
+        cout << "Provide the value of beta: ";
+        cin >> beta;
+        cout << endl;
 
-    // Take the values of alpha and beta from user
-    cout << "Enter a value to control contrast: " << endl;
-    cin >> alpha;
-    cout << "Enter a value to control brighness: " << endl;
-    cin >> beta;
-
-    for( int y = 0; y < src.rows; y++){
-        for( int x = 0; x < src.cols; x++){
-            for( int c = 0; c<3; c++){
-                final.at<Vec3b>(y,x)[c] = ( alpha * src.at<Vec3b>(y,x)[c] + beta );
-            }
-
+        if(!src.data){
+            cout << "No valid image file!" << endl;
         }
-    }
 
-    namedWindow( "Final Image Display",CV_WINDOW_AUTOSIZE);
-    imshow( "FInal Image Display", final);
-    waitKey(0);
-    return 0;   
+        for( int y = 0;y < src.rows; y++){
+            for (int x = 0; x < src.cols; x++ ){
+                for( int c = 0; c < 3; c++){
+                    final.at<Vec3b>(y,x)[c] = saturate_cast<uchar>(alpha * src.at<Vec3b>(y,x)[c] + beta);
+                }
+            }
+        }
 
+        namedWindow("Final image",CV_WINDOW_AUTOSIZE);
+        imshow("Final image", final);
+        waitKey(0);
+        return 0;
 }
