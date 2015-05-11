0fd6aa674802392c2193315a7493395c2054b29c
dhaivat666 <dhaivat1994@gmail.com>
2015-05-11 10:15:47 +0530

    Parted if condition in LoadModifyAndSave.cpp.

--- IntroductionToOpencv/LoadModifyAndSaveImage/LoadModifyAndSave.cpp
+++ IntroductionToOpencv/LoadModifyAndSaveImage/LoadModifyAndSave.cpp
@@ -1,33 +1,32 @@
 #include <stdio.h>
-#include <cv.h>
 #include <opencv2/core/core.hpp>
+#include <cv.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
 using namespace std;
 using namespace cv;
 
-int main( int argc, char** argv ){
+int main(int argc, char** argv){
 
-    char* imagename = argv[1];
-    Mat image;
-    image = imread( imagename, CV_LOAD_IMAGE_COLOR);
+    if(argc!=2){
 
-
-    if ( argc != 2 || !image.data ){
-
-        printf (" No image data! ");
-        return -1;
+        cout << "Usage: LoadModifyAndSave <image path>" << endl;
+        return 0;
     }
-
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    if(!image.data){
+        cout << "Input data is not valid!" << endl;
+        return 0;
+    }
     Mat gray_image;
-    cvtColor( image , gray_image, CV_BGR2GRAY);
-    imwrite( "./gray_lena.jpg", gray_image );
-    namedWindow( imagename, CV_WINDOW_AUTOSIZE );
-    namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
-    imshow( imagename, image);
-    imshow( "Gray image", gray_image);
+    cvtColor(image, gray_image,CV_BGR2GRAY);
+    imwrite("./gray_lena.jpg",gray_image);
+    namedWindow("Display Color Image",WINDOW_AUTOSIZE);
+    namedWindow("Display Gray Image",WINDOW_AUTOSIZE);
+    imshow("Display Color Image",image);
+    imshow("Display Gray Image",gray_image);
     waitKey(0);
     return 0;
-
 }
