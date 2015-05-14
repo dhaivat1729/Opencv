40a30f8ca1dfa5a70ec3c955c1c65b33743d4031
dhaivat666 <dhaivat1994@gmail.com>
2015-05-14 16:31:41 +0530

    Makefiles.

---
+++ Highgui/Trackbar2/CMakeFiles/Trackbar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Trackbar.dir/Trackbar.cpp.o"
+  "Trackbar.pdb"
+  "Trackbar"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Trackbar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
