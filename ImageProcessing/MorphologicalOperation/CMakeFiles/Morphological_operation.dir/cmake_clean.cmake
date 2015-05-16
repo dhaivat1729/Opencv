ad9361c46990adfaa5ed0b43b6a096a02079c4fc
dhaivat666 <dhaivat1994@gmail.com>
2015-05-17 03:07:05 +0530

    Makefiles.

---
+++ ImageProcessing/MorphologicalOperation/CMakeFiles/Morphological_operation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Morphological_operation.dir/Morphological_operation.cpp.o"
+  "Morphological_operation.pdb"
+  "Morphological_operation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Morphological_operation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
