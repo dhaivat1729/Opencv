b3f01c4522187d95e40bba58fd1e1a0b97ea5711
dhaivat666 <dhaivat1994@gmail.com>
2015-05-16 15:02:41 +0530

    Makefiles.

---
+++ ImageProcessing/ErosionAndDilation/Dilation/CMakeFiles/Dilation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Dilation.dir/Dilation.cpp.o"
+  "Dilation.pdb"
+  "Dilation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Dilation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
