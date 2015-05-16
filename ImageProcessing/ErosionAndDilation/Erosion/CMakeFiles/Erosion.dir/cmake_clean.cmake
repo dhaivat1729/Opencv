a85f47f011e63c0ed84b830f917e868d6bcbde81
dhaivat666 <dhaivat1994@gmail.com>
2015-05-16 09:43:23 +0530

    Makefiles.

---
+++ ImageProcessing/ErosionAndDilation/Erosion/CMakeFiles/Erosion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Erosion.dir/Erosion.cpp.o"
+  "Erosion.pdb"
+  "Erosion"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Erosion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
