28e25f95187dac67c7a30cbf10227aa92f3b02a2
dhaivat666 <dhaivat1994@gmail.com>
2015-05-17 03:32:31 +0530

    Added trackbars to perform morphological operations.

--- ImageProcessing/MorphologicalOperation/Morphological_operation.cpp
+++ ImageProcessing/MorphologicalOperation/Morphological_operation.cpp
@@ -7,7 +7,28 @@
 using namespace cv;
 using namespace std;
 
+// Images
 Mat image, final;
+
+// global variables
+int operation;
+const int operation_max = 4;
+int morph_elem;
+int elem_type;
+const int elem_type_max = 2;
+int elem_size;
+const int elem_size_max = 10;
+
+// trackbar callback function
+
+void morphology( int, void* ){
+
+    morph_elem = operation + 2;
+    Mat element = getStructuringElement(elem_type, Size(2*elem_size + 1, 2*elem_size + 1), Point(-1, -1));
+    morphologyEx(image, final, morph_elem, element);
+    imshow("Morphological operation: ", final);
+}
+
 
 int main(int argc, char** argv){
 
@@ -24,26 +45,22 @@ int main(int argc, char** argv){
         cout << "No valid image data." << endl;
     }
 
-    // Variables for type of morphological operation
-    int operation;
-    int morph_operator;
-    cout << "Operator: \n 0. Opening \n 1. Closing \n 2. Gradient \n 3. Top hat \n 4. Black hat" << endl;
-    cin >> morph_operator;
-    operation = morph_operator + 2;
-
-    // morph_operator
-    int morph_elem;
-    cout << "Element: \n 0. MORPH_RECT \n 1. MORPH_CROSS \n 2. MORPH_ELLIPSE";
-    cin >> morph_elem;
-
-    // Element size
-    int element_size = 3;
-
-    // Defining structuring element
-    Mat element = getStructuringElement(morph_elem, Size(element_size, element_size), Point(-1, -1));
-    morphologyEx(image, final, operation, element);
+    // creating window
     namedWindow("Morphological operation: ", CV_WINDOW_AUTOSIZE);
-    imshow("Morphological operation: ", final);
+
+    char* morph_operation =  "Operator: \n 0. Opening \n 1. Closing \n 2. Gradient \n 3. Top hat \n 4. Black hat";
+
+    char* morphological_type = "Element: \n 0. MORPH_RECT \n 1. MORPH_CROSS \n 2. MORPH_ELLIPSE";
+
+    char* mask_size = "Kernel size: ";
+
+    // Creating trackbars
+    createTrackbar(morph_operation, "Morphological operation: ", &operation, operation_max, morphology);
+
+    createTrackbar(morphological_type, "Morphological operation: ", &elem_type, elem_type_max, morphology);
+
+    createTrackbar(mask_size, "Morphological operation: ", &elem_size, elem_size_max, morphology);
+
     waitKey(0);
     return 0;
 }
