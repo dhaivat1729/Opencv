010100199df8137ddcde59cbeb16f1da1359c3da
dhaivat666 <dhaivat1994@gmail.com>
2015-05-21 23:01:04 +0530

    Makefiles.

---
+++ ImageProcessing/Thresholding/CMakeFiles/Thresholding.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Thresholding.dir/Thresholding.cpp.o"
+  "Thresholding.pdb"
+  "Thresholding"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Thresholding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
