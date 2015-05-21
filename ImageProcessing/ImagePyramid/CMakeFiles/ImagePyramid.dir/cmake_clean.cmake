109e8b44446eb1a81e997aa2b84695fb3b61f5fa
dhaivat666 <dhaivat1994@gmail.com>
2015-05-21 23:28:18 +0530

    Makefiles.

---
+++ ImageProcessing/ImagePyramid/CMakeFiles/ImagePyramid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ImagePyramid.dir/ImagePyramid.cpp.o"
+  "ImagePyramid.pdb"
+  "ImagePyramid"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ImagePyramid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
