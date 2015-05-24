8ca925517c05719913f0ee0f28435439b26a2a19
dhaivat666 <dhaivat1994@gmail.com>
2015-05-24 20:58:26 +0530

    Autogenerates makefiles and dependencies.

---
+++ ImageProcessing/LinearFilters/CMakeFiles/LinearFilters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LinearFilters.dir/LinearFilters.cpp.o"
+  "LinearFilters.pdb"
+  "LinearFilters"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LinearFilters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
