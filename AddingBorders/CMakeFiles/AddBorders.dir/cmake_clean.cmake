4a4f07fa8da359327b8ffa9849bb6b11e7647201
dhaivat666 <dhaivat1994@gmail.com>
2015-06-16 10:43:35 +0530

    Makefiles.

---
+++ AddingBorders/CMakeFiles/AddBorders.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/AddBorders.dir/AddBorders.cpp.o"
+  "AddBorders.pdb"
+  "AddBorders"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/AddBorders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
